2cf844a9b0b4967d269e8526474df5502a93026a
Ninjiator <alex.megakep@gmail.com>
2025-10-17 12:17:03 +0200

    completed task

---
+++ lesson_09/homework_09.py
@@ -0,0 +1,31 @@
+class Diamond:
+    def __init__(self, side, angle_a):
+        self.side = float(side)
+        self.angle_a = angle_a
+        self.angle_b = 180.0 - angle_a
+
+    def __setattr__(self, name, value):
+        if name == 'side':
+            if value <= 0:
+                raise AttributeError(f'self.side should be bigger that zero')
+            else:
+                object.__setattr__(self, name, value)
+        elif name == 'angle_a':
+            if value <= 0:
+                raise AttributeError(f'angle_a should be bigger that zero')
+            else:
+                object.__setattr__(self, name, value)
+                self.angle_b = 180.0 - self.angle_a
+        else:
+            object.__setattr__(self, name, value)
+
+    def __str__(self):
+        return f'side is {self.side}, angle_a is {self.angle_a}, angle_b is {self.angle_b}'
+
+
+d_1 = Diamond(5, 120)
+print(d_1)
+print('-' * 80)
+d_2 = Diamond(1, 10)
+print(d_2)
+print('-' * 80)
