96b9f4a95fcc18d0bc4475ff2cbfd990686118c4
Ninjiator <alex.megakep@gmail.com>
2025-10-17 13:04:17 +0200

    fix for angle_a + angle_b can be bigger than 180

--- lesson_09/homework_09.py
+++ lesson_09/homework_09.py
@@ -2,7 +2,7 @@ class Diamond:
     def __init__(self, side, angle_a):
         self.side = float(side)
         self.angle_a = angle_a
-        self.angle_b = 180.0 - angle_a
+        self.__angle_b = 180.0 - angle_a
 
     def __setattr__(self, name, value):
         if name == 'side':
@@ -16,6 +16,8 @@ class Diamond:
             else:
                 object.__setattr__(self, name, value)
                 self.angle_b = 180.0 - self.angle_a
+        elif name == 'angle_b' and value + self.angle_a > 180:
+            raise AttributeError(f'angle_a + angle_a cannot be > 180')
         else:
             object.__setattr__(self, name, value)
 
@@ -24,8 +26,10 @@ class Diamond:
 
 
 d_1 = Diamond(5, 120)
-print(d_1)
+print('d_1 - ', d_1)
 print('-' * 80)
 d_2 = Diamond(1, 10)
-print(d_2)
+print('d_2 - ',d_2)
 print('-' * 80)
+d_2.angle_b = 180.0
+print('d_2 - ', d_2)
