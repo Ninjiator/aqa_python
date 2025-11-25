aff1ca83c2a8b3ed3d19a9bdf6394bfc3d93688c
laptop <alex.megakep@gmail.com>
2025-11-25 09:56:07 +0100

    task completed

--- lesson_19/homework_19_2.py
+++ lesson_19/homework_19_2.py
@@ -1,18 +1,20 @@
 import requests
 
-base_url = 'http://127.0.0.1:8000'
-url_post = base_url + '/upload'
-with open("image.jpg", "rb") as file:
-    files = {"image": ("image.jpg", file, "image/jpeg")}
+base_url = "http://127.0.0.1:8080"
 
-    response_post = requests.post(url_post, files=files)
-    data = response_post.json()
+post_path = '/upload'
 
-print(data)
+with open("images.jpeg", "rb") as image_file:
+    image_data = image_file.read()
+    file = {"image" : ('images.jpeg', image_data, "image/jpeg")}
+response_post = requests.post(base_url + post_path, files = file)
+print(f'Post results: Status is {response_post.status_code},\nText: {response_post.text}')
 
-url_get = 'http://127.0.0.1:8080/image/image.jpg'
-response_get = requests.get(url_get)
+get_path = f'/image/{image_file.name}'
+header_text = {'Content-Type' : 'text'}
+response_get = requests.get(base_url + get_path, headers = header_text)
+print(f'Get results: Status is {response_get.status_code},\nText: {response_get.text}')
 
-
-
-
+delete_path = f"/delete/{image_file.name}"
+delete_response = requests.delete(base_url + delete_path)
+print(f'Delete results: Status is {delete_response.status_code},\nText: {delete_response.text}')
