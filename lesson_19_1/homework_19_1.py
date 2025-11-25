6eefffb18adb6d7a8c8dce595be506f8e5d6fc94
laptop <alex.megakep@gmail.com>
2025-11-25 16:42:15 +0100

    find a right approach

--- lesson_19_1/homework_19_1.py
+++ lesson_19_1/homework_19_1.py
@@ -8,16 +8,13 @@ def get_random_image(data):
 base_url = 'https://images-api.nasa.gov/search?q=mars&media_type=image'
 response = requests.get(base_url)
 data = response.json()
-#print(get_random_image(data))
-
-#x = random.choice(data['collection']['items'])
-#print(x['href'])
-#print(data['collection']['items'][0]['href'])
-#for item in data['collection']['items']:
-#    print('-'*10)
-#    print(item['links'])
 
 r = requests.get(get_random_image(data))
-print(r.content)
+print(r.json()[0])
+image_url = str(r.json()[0])
+response_get = requests.get(image_url)
+
+
+
 with open('image.jpg', 'wb') as nasa_image:
-    nasa_image.write(r.content)
+    nasa_image.write(response_get.content)
