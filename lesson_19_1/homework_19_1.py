3ed9301638fb4df8f9a123144544d7c065e93e85
laptop <alex.megakep@gmail.com>
2025-11-25 17:20:21 +0100

    task completed

--- lesson_19_1/homework_19_1.py
+++ lesson_19_1/homework_19_1.py
@@ -1,20 +1,30 @@
 import requests
 import random
 
-def get_random_image(data):
+def get_random_image_url(data : dict) -> str:
     image = random.choice(data['collection']['items'])
-    return image['href']
+    get_request = requests.get(image['href'])
+    return get_request.json()[0]
+
+def download_image(response : requests.Response, name : str) -> None:
+     with open(f'{name}.jpg', 'wb') as nasa_image:
+        nasa_image.write(response.content)
+
 
 base_url = 'https://images-api.nasa.gov/search?q=mars&media_type=image'
-response = requests.get(base_url)
-data = response.json()
+base_response = requests.get(base_url)
+base_data = base_response.json()
 
-r = requests.get(get_random_image(data))
-print(r.json()[0])
-image_url = str(r.json()[0])
-response_get = requests.get(image_url)
+image_1 = get_random_image_url(base_data)
+image_2 = get_random_image_url(base_data)
+image_3 = get_random_image_url(base_data)
+
+image_response_1 = requests.get(image_1)
+image_response_2 = requests.get(image_2)
+image_response_3 = requests.get(image_3)
+
+download_image(image_response_1, 'image_1')
+download_image(image_response_2, 'image_2')
+download_image(image_response_3, 'image_3')
 
 
-
-with open('image.jpg', 'wb') as nasa_image:
-    nasa_image.write(response_get.content)
