f508ff785bdd3814009b38447063ef5610e3c006
Ninjiator <alex.megakep@gmail.com>
2025-11-29 12:48:40 +0100

    completed

--- lesson_20/homework_20.py
+++ lesson_20/homework_20.py
@@ -1,35 +1,44 @@
-from datetime import datetime
+from argparse import FileType
+import logging
+from datetime import datetime, timedelta
 
 
-def filter_log_by_key(file, key):
-    with open(file, 'r') as f:
+def filter_log_by_key(log_file : FileType, key_time_stamp : str) -> list[datetime]:
+    with open(log_file, 'r') as f:
         log_data = f.readlines()
     log_for_key = []
     for row in log_data:
-        if key in row:
-            log_for_key.append(row)
-    with open('log_by_key.txt', 'w') as file:
-        file.writelines(log_for_key)
+        if key_time_stamp in row:
+            time_stamp_index = row.find('Timestamp') + len('Timestamp')
+            time_stamp_str = row[time_stamp_index + 1:(time_stamp_index + len('Timestamp'))]
 
-def log_requirements_analysis(file):
-    """
-для кожного випадку де heartbeat більше 31 сек але менше 33 логувало WARNING в файл hb_test.log
-для кожного випадку де heartbeat більше рівно 33 логувало ERROR в файл hb_test.log"""
+            time_stamp = datetime.strptime(time_stamp_str, "%H:%M:%S")
+            log_for_key.append(time_stamp)
+    return log_for_key
 
-    with open(file, 'r') as f:
-        log_data = f.readlines()
-    for row in log_data:
-        time_stamp_index = row.find('Timestamp') + len('Timestamp')
-        time_stamp_str = row[time_stamp_index + 1:(time_stamp_index + len('Timestamp'))]
-        print('->',time_stamp_str)
-        time_stamp = datetime.strptime(time_stamp_str, "%H:%M:%S")
-        print(time_stamp)
+def log_requirements_analysis(list_of_datetime: list[datetime]):
+    index = 1
+    while index <= len(list_of_datetime) - 1:
+        time_diff = list_of_datetime[index - 1] - list_of_datetime[index]
+        if time_diff > timedelta(seconds=31) and time_diff < timedelta(minutes=33):
+            logger.warning(f'time_diff between {list_of_datetime[index - 1].time()} and {list_of_datetime[index].time()} is {time_diff}')
 
-
+        if time_diff > timedelta(seconds=33):
+            logger.error(f'time_diff between {list_of_datetime[index - 1].time()} and {list_of_datetime[index].time()} is {time_diff}')
+        index += 1
 
 
+
+logger = logging.getLogger('Log_analyzer')
+logger.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler('hb_test.log')
+formatter = logging.Formatter("%(levelname)s - %(message)s")
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
 
 key = 'TSTFEED0300|7E3E|0400'
-filter_log_by_key('hblog.txt', key)
+list_time_stamps = filter_log_by_key('hblog.txt', key)
+log_requirements_analysis(list_time_stamps)
 
-log_requirements_analysis('log_by_key.txt')
