28f282c4efb6aa50478ac40c539efa3e7bd5560d
Ninjiator <alex.megakep@gmail.com>
2025-12-07 13:49:26 +0100

    hm done

--- lesson_22/homework_22.py
+++ lesson_22/homework_22.py
@@ -1,57 +1,81 @@
-from Models.models import Student, Course, Relation, Base
-from sqlalchemy.orm import sessionmaker
+import random
+from Models.models import Student, Course, Base
+from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy import create_engine
 from faker import Faker
-import random
 
 connection_string = "sqlite:///C:/Oleksii_Docs/aqa_python/lesson_22/sqllite_db"
 engine = create_engine(connection_string)
+fake = Faker()
 
-""""
-1. Виконання базових операцій: Напишіть програму, яка додає нового студента до бази даних та додає його до певного курсу. 
--Переконайтеся, що ці зміни коректно відображаються у базі даних.
-2. Запити до бази даних: 
--Напишіть запити до бази даних, які повертають інформацію про студентів, зареєстрованих на певний курс, або курси, на які зареєстрований певний студент.
-3. Оновлення та видалення даних: 
--Реалізуйте можливість оновлення даних про студентів або курси, а також видалення студентів з бази даних."""
 
-def init_students(stud_num : int) -> list[Student]:
-    fake = Faker()
+def create_students(stud_num: int) -> list[Student]:
     stud_list = []
     for s in range(stud_num):
         student = Student(student_name=fake.name())
         stud_list.append(student)
     return stud_list
 
-def init_courses() -> list[Course]:
+def create_courses() -> list[Course]:
     l_courses = [Course(course_name='Python for AQA'),
-                       Course(course_name='C++ for GameDev'),
-                       Course(course_name='Human Resources'),
-                       Course(course_name='Data Analysis'),
-                       Course(course_name='DevOps')]
+                Course(course_name='C++ for GameDev'),
+                Course(course_name='Human Resources'),
+                Course(course_name='Data Analysis'),
+                Course(course_name='DevOps')]
     return l_courses
 
-def init_relations(courses : list[Course], students : list[Student]) -> list[Relation]:
-    relations = []
-    for student in students:
-        r = Relation(s_id=student.student_id, c_id=random.choice(courses).course_id)
-        relations.append(r)
-    return relations
+def allocate_students_on_course(studs_list: list[Student], courses_list: list[Course]) -> None:
+    index = 0
+    while index < len(studs_list):
+        rand_course = random.choice(courses_list)
+        rand_student = random.choice(studs_list)
+        rand_student.enroll(rand_course)
+        index = index + 1
 
+def get_student_courses(session : Session, student_id: int) -> list[Course]:
+    student = session.get(Student, student_id)
+    if student is None:
+        return []
+    return student.courses
+
+def get_course_students(session : Session, course_id : int) -> list[Student]:
+    course = session.get(Course, course_id)
+    if course is None:
+        return []
+    return course.students
+
+def delete_course(session : Session, course_id : int) -> bool:
+    course = session.get(Courses, course_id)
+    if course is None:
+        return False
+    session.delete(course)
+    session.commit()
+    return True
+
+def delete_student(session : Session, student_id : int) -> bool:
+    student = session.get(Student, student_id)
+    if student is None:
+        return False
+    session.delete(student)
+    session.commit()
+    return True
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
     SessionLocal = sessionmaker(bind=engine)
     session = SessionLocal()
 
-    list_of_students = init_students(20)
+    list_of_students = create_students(20)
     session.add_all(list_of_students)
     session.commit()
 
-    list_of_courses = init_courses()
+    list_of_courses = create_courses()
     session.add_all(list_of_courses)
     session.commit()
 
-    list_of_relations = init_relations(list_of_courses, list_of_students)
-    session.add_all(list_of_relations)
+    allocate_students_on_course(list_of_students, list_of_courses)
     session.commit()
+
+
+    print(*get_student_courses(session, 7))
+    print(*get_course_students(session, 5))
