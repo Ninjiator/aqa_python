dca8a2ec65560a903597f222502296ea977c85d4
Ninjiator <alex.megakep@gmail.com>
2026-01-17 15:31:24 +0100

    commit before switching to branch with hm29

--- lesson_26_27_28/HillelAuto/Pages/MyProfilePage.py
+++ lesson_26_27_28/HillelAuto/Pages/MyProfilePage.py
@@ -1,9 +1,10 @@
 from playwright.sync_api import Page
 from HillelAuto.Forms.LoginForm import LoginForm
 
-
-
 class MyProfilePage:
 
     def __init__(self, login_page : LoginForm):
-        pass
+        self.page = login_page.page
+
+
+    def 
